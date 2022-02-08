5a6967d45204079469e52d0794749d6385737203
AYehia0 <ahmedyehia611@gmail.com>
2022-02-08 22:00:12 +0200

    User routes : updated!!!

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
+const auth = require('../middlewares/auth.js')
 
 // the controller
 const userCont = require('../controller/userController.js')
 
 // register a user
 router.post('/register', userCont.registerUser)
+
+// login a user
+router.post('/login', userCont.loginUser)
+
+// show your profile
+router.get('/profile', auth.userAuth , userCont.getUserProfile)
 
 // exporting
 module.exports = router
