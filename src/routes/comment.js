cda9b056c8e2724e47b07bd7620b0716b3f5ba1f
AYehia0 <ahmedyehia611@gmail.com>
2022-02-22 16:11:57 +0200

    all the routes related to the comments thing : might edit later

---
+++ src/routes/comment.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const auth = require('../middlewares/auth.js')
+
+// the controller
+const commentCont = require('../controller/userComController')
+
+// write a review
+router.post('/comment/:id', auth.userAuth, commentCont.giveComment)
+
+// edit a comment
+// we can pass the comment id or the request id : as it's just one-to-one
+router.put('/comment/:id', auth.userAuth, commentCont.editComment)
+
+// delete a comment
+router.delete('/comment/:id', auth.userAuth, commentCont.deleteComment)
+
+// get all the comment of a request
+// by doing this : i make sure to sep the comments from the request itself, means you will need extra request to load all the comments
+router.get('/comment/:id', auth.userAuth, commentCont.getComments)
+
+
+// exporting
+module.exports = router
