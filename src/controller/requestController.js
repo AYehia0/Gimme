6093f92687888ac9389c53033ca6cd8ed69937b6
AYehia0 <ahmedyehia611@gmail.com>
2022-02-23 08:18:52 +0200

    close and edit request

--- src/controller/requestController.js
+++ src/controller/requestController.js
@@ -1,4 +1,6 @@
 const Request = require('../models/Request')
+const Comment = require('../models/Comment')
+const User = require('../models/User')
 
 // create a new request
 const openRequest = async (req, res) => {
@@ -38,11 +40,15 @@ const openRequest = async (req, res) => {
 // under one contition, the mod isn't choosen
 const editRequest = async (req, res) => {
     try {
+        const user = req.user
         const reqId = req.params.id
 
         // finding the request
         // TODO : check the request status aka state
         const checkReq = await Request.findById(reqId)
+
+        if (!checkReq.userId.equals(user._id))
+            throw new Error("Can't perform this action !!!")
 
         if (! checkReq)
             throw new Error("Nothing to edit")
@@ -50,7 +56,7 @@ const editRequest = async (req, res) => {
         if (checkReq.state != "on")
             throw new Error("Can't edit a closed/fulfilled request")
 
-        const requestToEdit = await Request.findByIdAndUpdate(reqId, req.body)
+        await Request.findByIdAndUpdate(reqId, req.body)
 
         // edit it 
         res.send({
@@ -68,16 +74,34 @@ const editRequest = async (req, res) => {
     }
 }
 
-// create a new request
+// delete a request : can't delete a request if the state is not closed
+// TODO : edit
 const deleteRequest = async (req, res) => {
     try {
+        const user = req.user
         const reqId = req.params.id
 
         // finding the request
-        const requestToDel = await Request.findByIdAndRemove(reqId)
+        const requestToDel = await Request.findById(reqId)
+
+        if (!requestToDel.userId.equals(user._id))
+            throw new Error("Can't perform this action !!!")
 
         if (! requestToDel)
             throw new Error("Nothing to delete")
+        
+        if (requestToDel.state != "closed")
+            throw new Error("Can't delete : must be closed")
+
+        // delete all the comments : oh boi
+        const commentIDs = requestToDel.participants.map(part => {
+            return part.commentId
+        })
+
+        await Comment.deleteMany({commentId : commentIDs})
+
+        // delete the actual request
+        await Request.deleteOne({_id : reqId})
 
         res.send({
             status: true,
@@ -100,6 +124,56 @@ const closeRequest = async (req, res) => {
     try {
         const user = req.user
 
+        // the request id 
+        const reqId = req.query.reqId
+
+        // the id of the choosen one
+        const userChoosen = req.query.modId
+
+        // check if the request is valid : auth + didn't expire
+        const checkRequest = await Request.findById(reqId)
+
+        if (!checkRequest.userId.equals(user._id))
+            throw new Error("Can't perform this action !!!")
+
+        if (!checkRequest)
+            throw new Error("Request doesn't exist, are you sure it exists ?")
+        
+        if (checkRequest.state !== "on")
+            throw new Error("It's already closed, fulfilled or deleted !!!")
+        
+        // user exists : LOL
+        const modInDB = await User.findById(userChoosen) 
+        if (! modInDB)
+            throw new Error("User not found")
+        
+        // check if the mod is one of the comments
+        const userChoosenInd = checkRequest.participants.findIndex((comment) => {
+            return comment.userId.equals(userChoosen)
+        })
+        if (userChoosenInd == -1)
+            throw new Error("User not found in comments")
+
+        // make it MOD
+        checkRequest.mod = userChoosen
+
+        // change the state of the request to fulfilled
+        checkRequest.state = "fulfilled" 
+
+        // change the comment of the user as MOD : true
+        await Comment.findByIdAndUpdate(checkRequest.participants[userChoosenInd].commentId, {
+            mod : true
+        })
+
+        await checkRequest.save()
+
+        res.send({
+            status: true,
+            message: "MOD has been choosen",
+            data: checkRequest
+        })
+    
+        // TODO : send to the other part to verify
         
     } catch (e) {
         const message = e.message
