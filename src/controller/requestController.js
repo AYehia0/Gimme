2e0a231b7165a5251107949a1ee103e57dca5cb4
AYehia0 <ahmedyehia611@gmail.com>
2022-03-11 01:47:37 +0200

    added some status codes

--- src/controller/requestController.js
+++ src/controller/requestController.js
@@ -28,13 +28,14 @@ const openRequest = async (req, res) => {
 
     } catch (e) {
         let message = e.message
+        let statusCode = 400
         if (message.includes("Can't extract geo keys"))
             message = "Invalid Syntax : Invalid location format"
 
         if (message.includes("Request validation failed"))
-            message = "Invalid Syntax : Check the docs"
+            message = "Invalid Syntax : There are some missing fields"
         
-        res.send({
+        res.status(statusCode).send({
             status: false,
             message: message,
             data: ""
@@ -46,6 +47,7 @@ const openRequest = async (req, res) => {
 // you can edit the title, body, locations, price and date 
 // under one contition, the mod isn't choosen
 const editRequest = async (req, res) => {
+    let statusCode = 400
     try {
         const user = req.user
         const reqId = req.params.id
@@ -54,14 +56,20 @@ const editRequest = async (req, res) => {
         // TODO : check the request status aka state
         const checkReq = await Request.findById(reqId)
 
-        if (!checkReq.userId.equals(user._id))
+        if (! checkReq){
+            statusCode = 404
+            throw new Error("Corrupted ID or Request not found")
+        }
+
+        if (!checkReq.userId.equals(user._id)){
+            statusCode = 403
             throw new Error("Can't perform this action !!!")
+        }
 
-        if (! checkReq)
-            throw new Error("Nothing to edit")
-
-        if (checkReq.state != "on")
+        if (checkReq.state != "on"){
+            statusCode = 403
             throw new Error("Can't edit a closed/fulfilled request")
+        }
 
         await Request.findByIdAndUpdate(reqId, req.body)
 
@@ -72,8 +80,10 @@ const editRequest = async (req, res) => {
             data: req.body
         })
     } catch (e) {
-        const message = e.message
-        res.send({
+        let message = e.message
+        if (message.includes('Cast to ObjectId failed'))
+            message = "Invalid ID"
+        res.status(statusCode).send({
             status: false,
             message: message,
             data: ""
@@ -84,6 +94,7 @@ const editRequest = async (req, res) => {
 // delete a request : can't delete a request if the state is not closed
 // TODO : edit
 const deleteRequest = async (req, res) => {
+    let statusCode = 400
     try {
         const user = req.user
         const reqId = req.params.id
@@ -91,14 +102,20 @@ const deleteRequest = async (req, res) => {
         // finding the request
         const requestToDel = await Request.findById(reqId)
 
-        if (!requestToDel.userId.equals(user._id))
-            throw new Error("Can't perform this action !!!")
-
-        if (! requestToDel)
-            throw new Error("Nothing to delete")
+        if (! requestToDel){
+            statusCode = 404
+            throw new Error("Corrupted ID or Request not found")
+        }
         
-        if (requestToDel.state != "closed")
+        if (!requestToDel.userId.equals(user._id)){
+            statusCode = 403
+            throw new Error("Can't perform this action !!!")
+        }
+
+        if (requestToDel.state != "closed"){
+            statusCode = 403
             throw new Error("Can't delete : must be closed")
+        }
 
         // delete all the comments : oh boi
         const commentIDs = requestToDel.participants.map(part => {
@@ -116,8 +133,8 @@ const deleteRequest = async (req, res) => {
             data: ""
         })
     } catch (e) {
-        const message = e.message
-        res.send({
+        let message = e.message
+        res.status(statusCode).send({
             status: false,
             message: message,
             data: ""
@@ -128,6 +145,7 @@ const deleteRequest = async (req, res) => {
 // close a request
 // by choosing the MOD
 const closeRequest = async (req, res) => {
+    let statusCode = 400
     try {
         const user = req.user
 
@@ -140,14 +158,20 @@ const closeRequest = async (req, res) => {
         // check if the request is valid : auth + didn't expire
         const checkRequest = await Request.findById(reqId)
 
-        if (!checkRequest.userId.equals(user._id))
+        if (!checkRequest.userId.equals(user._id)){
+            statusCode = 403
             throw new Error("Can't perform this action !!!")
+        }
 
-        if (!checkRequest)
+        if (!checkRequest){
+            statusCode = 404
             throw new Error("Request doesn't exist, are you sure it exists ?")
+        }
         
-        if (checkRequest.state !== "on")
+        if (checkRequest.state !== "on"){
+            statusCode = 409
             throw new Error("It's already closed, fulfilled or deleted !!!")
+        }
         
         // user exists : LOL
         const modInDB = await User.findById(userChoosen) 
@@ -183,8 +207,10 @@ const closeRequest = async (req, res) => {
         // TODO : send to the other part to verify
         
     } catch (e) {
-        const message = e.message
-        res.send({
+        let message = e.message
+        if (message.includes('Cast to ObjectId failed'))
+            message = "Corrupted ID : Check the ID for both the request and the MOD"
+        res.status(statusCode).send({
             status: false,
             message: message,
             data: ""
