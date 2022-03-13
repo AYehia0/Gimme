c0303678cbfe2f1d03b1799e350990f3d76fe47a
AYehia0 <ahmedyehia611@gmail.com>
2022-03-13 22:37:15 +0200

    message controller has been added [need work]

---
+++ src/controller/userMessageController.js
@@ -0,0 +1,81 @@
+const Room = require('../models/Room')
+const Message = require('../models/Message')
+
+const addMsgToChat = async (req, res) => {
+    try {
+
+        // the user to send
+        const sender = req.user
+        const roomId = req.params.id
+        const message = req.body.text
+
+        // check if the chatRoom exists or not
+        if (! message)
+            throw new Error("Message is required")
+
+        const chatRoomExsits = await Room.findById(roomId)
+        if (! chatRoomExsits)
+            throw new Error("Chat doesn't exist")
+
+        // Create a message 
+        const msg = await Message.addMessageToChat(roomId, message, sender._id)
+
+        // emit to the socket
+        global.io.in(roomId).emit('new-msg', { message: msg })
+
+        res.send({
+            status: true,
+            message: "",
+            data: msg
+        })
+        
+    } catch (e) {
+        const message = e.message
+        res.send({
+            status: false,
+            message: message,
+            data: ""
+        })
+    }
+}
+
+const getChatMessages = async (req, res) => {
+    try {
+
+        // the user to send
+        const roomId = req.params.id
+
+        const opt =  {
+            page: parseInt(req.query.page) || 0,
+            limit: parseInt(req.query.limit) || parseInt(process.env.CHAT_SIZE),
+        } 
+
+        // make sure that the user is : maker or MOD
+        // ...
+        const chatConv = await Message.getRoomMessages(roomId, opt)
+
+        // emit to the socket
+        //global.io.in(roomId).emit('new-msg', { message: msg })
+
+        res.send({
+            status: true,
+            message: "",
+            data: chatConv 
+        })
+
+    } catch (e) {
+        let message = e.message
+        res.send({
+            status: false,
+            message: message,
+            data: ""
+        })
+    }
+}
+
+
+
+module.exports =  {
+    addMsgToChat,
+    getChatMessages,
+}
