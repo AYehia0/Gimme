480dc8ca466b57028ec9460a0003d59d23bbd28e
AYehia0 <ahmedyehia611@gmail.com>
2022-03-14 20:25:57 +0200

    Phone number is unique, edited the response text

--- src/controller/userRegController.js
+++ src/controller/userRegController.js
@@ -20,7 +20,7 @@ const registerUser = async (req, res) => {
     })
     
   } catch (e) {
-    let message = e.message.includes('E11000') ? "Email Already Exists" : e.message
+    let message = e.message.includes('E11000') ? "Email/Phone Already Exists" : e.message
     res.send({
       status: false,
       message: message,
