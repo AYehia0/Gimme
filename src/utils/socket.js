8ba57cc2179cb2cb66551e69979a078f4b85fb1f
AYehia0 <ahmedyehia611@gmail.com>
2022-03-17 14:48:54 +0200

    commented on how to test for future updates

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,17 @@
 // simple class to handle sockets between users : user joins, disconnects, ... etc
+/*
+How to test : 
+
+    - Create a frontend
+    - When the user connects to the chat : /get-room-msgs (with params) when connected to pull the latest msgs
+    - When a user send a msg to the other party this api is called : /create-msg/roomId
+        - The msg is created and stored in the db, then the socket emits
+    
+    Notes : 
+        - The sockets are wrapped inside a secure api that requires the user to be loggedin
+        - Only certain users can create sockets, check the docs
+
+*/
 class MainSocket {
 
     // the 2 users communicating
@@ -7,11 +20,32 @@ class MainSocket {
     // handles the connection stats : disconnect, connect, join, leave, .... etc
     connection (client) {
 
-       // TODO : user leaves 
-        // ...
+        // TODO : listen on new msgs
+
+        // user joins from the frontend
+        client.on("joins", (userId) => {
+
+            // add the user + socketId
+            this.addUserToParty(userId, client.id)
+        })
+
+        // disconnection
+        client.on("disconnect", () => {
+            this.removeUserFromParty()
+        })
 
     }
 
+    removeUserFromParty (){
+        this.party = this.party.filter((user) => user.socketId !== client.id)
+    }
+
+    addUserToParty (userId, socketId) {
+        this.party.push({
+            userId,
+            socketId
+        })
+    }
     joinUser (room, userJoinId) {
 
         // getting the user info
@@ -26,7 +60,6 @@ class MainSocket {
                 socketConnection.join(room)
         })
     }
-
 }
 
 module.exports = new MainSocket()
