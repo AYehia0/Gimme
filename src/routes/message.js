32929030f1f032afbeb497925eadcfcb0f69862b
AYehia0 <ahmedyehia611@gmail.com>
2022-03-22 23:58:49 +0200

    removed the api route : add-msg

--- src/routes/message.js
+++ src/routes/message.js
@@ -5,9 +5,6 @@ const chatAuth = require('../middlewares/chat')
 // the controller
 const messageCont = require('../controller/user.message')
 
-// create a message in a chat room(id)
-router.post('/create-msg/:id', auth.userAuth, chatAuth.chatAuth, messageCont.addMsgToChat)
-
 // get messages
 router.get('/get-room-msgs/:id', auth.userAuth, chatAuth.chatAuth, messageCont.getChatMessages)
 
