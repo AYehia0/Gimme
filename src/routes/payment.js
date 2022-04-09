d834c7d82496ab4d905dd80ddfe0ef10e9164859
AYehia0 <ahmedyehia611@gmail.com>
2022-04-09 13:24:40 +0200

    stripe routes

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,4 +1,7 @@
+const bodyParser = require("body-parser")
 const router = require('express').Router()
+//.use(bodyParser.raw({type: '*/*'}))
+const express = require("express")
 const auth = require('../middlewares/auth')
 
 // the controller
@@ -6,6 +9,12 @@ const paymentCont = require('../controller/user.payment')
 
 // retrive stripe's publishable key to be used in the frontend
 router.get("/config", auth.userAuth, paymentCont.getPublishKey)
+
+// create a session
+router.post("/create-stripe-session", auth.userAuth,  paymentCont.createStripeSession)
+
+// express.raw({type: 'application/json'})
+router.post("/webhook", paymentCont.customWebhook)
 
 // exporting
 module.exports = router
