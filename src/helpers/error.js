af2d556a9fb93e3027890b34c2bf968d26d9de5c
AYehia0 <ahmedyehia611@gmail.com>
2022-04-16 09:46:05 +0200

    helper functions : success and error are gathered for wide configs

---
+++ src/helpers/error.js
@@ -0,0 +1,16 @@
+export default {
+    login : "Not authorized",
+    auth : "Not authorized",
+    notFound : "User not found",
+    auth : "You're not allowed to perform this action",
+    invalid : {
+        missing : "Invalid Syntax : There are some missing fields",
+        id : "The given ID is either invalid or doesn't exist",
+        location : "Invalid geoJSON type :  { 'type' : 'Point' } is required or the coordinates are invalid"
+    }, 
+    request : {
+        delete : "You aren't allowed to delete a request with a state of fulfilled",
+        edit : "You aren't allowed to edit a request with a state of fulfilled/closed",
+        notfound : "You're trying to access a request that doesn't exist"
+    }
+}
--- src/helpers/success.js
+++ src/helpers/success.js
@@ -1,7 +1,13 @@
-function register () {
-    return "Success : User registered !!!"
-}
-
 export default {
-    register,
+    register : "Success : Account has been created.",
+    login : "Success : User has been logged in.",
+    logout : "Success : User has been logged out.",
+    edit : "Success : User's profile has been edited.",
+    uploadImg : "Profile picture has been updated succesfully",
+    request : {
+        added : "Success : request has been added !!!",
+        edit : "Success : request has been edited !!!",
+        delete : "Success : request has been deleted !!!",
+    }
+
 }
