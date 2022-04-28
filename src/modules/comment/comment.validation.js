51538c51f18151e5c4ab5582adefd4be9ce1dbcb
AYehia0 <ahmedyehia611@gmail.com>
2022-04-28 16:43:33 +0200

    fixed a bug adding a comment : negative price and time

--- src/modules/comment/comment.validation.js
+++ src/modules/comment/comment.validation.js
@@ -4,10 +4,10 @@ const validateComment = (rawData) => {
 
     const Comment = z.object({
         text: z.string().nonempty("Text field can't be empty").max(200),
-        price: z.number().negative().gt(0),
+        price: z.number().nonnegative().gt(0),
         time: z.object({
             unit: z.enum(["w", "d", "h"]).default("d"),
-            val: z.number().negative().gt(0)
+            val: z.number().nonnegative().gt(0)
         })
     })
 
