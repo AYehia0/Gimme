af3eb82308fc21a4942a8b73d23a39d34e3752a6
AYehia0 <ahmedyehia611@gmail.com>
2022-06-22 19:51:59 +0200

    changed the templates for the emails : password reset and verification

--- src/helpers/emailTemplate.js
+++ src/helpers/emailTemplate.js
@@ -1,10 +1,29 @@
-export default (user, token) => {
-  var mailOptions = {
+// The basic template which is being sent to the user as email, when requesting verification
+const verificationTemplate = (user, token) => {
+
+  return {
     from: "no-reply@example.com",
     to: user.email,
     subject: "Account Verification Link",
-    text: `Hello ${user.name}, \n\nPlease verify your account by clicking this like:\n\nhttp://${process.env.HOST_DEV}:${process.env.PORT}${process.env.MAIN_API}/${process.env.USER_API}/confirm?userId=${user._id}&token=${token.token}\n\nThank You :D`
+    text: `Hello ${user.name}, \n\nPlease verify your account by clicking this link:\n\nhttp://${process.env.HOST_DEV}:${process.env.PORT}${process.env.MAIN_API}/${process.env.USER_API}/confirm?userId=${user._id}&token=${token.token}\n\nThank You :D`
   }
 
-  return mailOptions
+}
+
+// when requesting password reset 
+const passwordResetTemplate = (user, token) => {
+
+  return {
+    from: "no-reply@example.com",
+    to: user.email,
+    subject: "Password Reset Link",
+    text: `Hello ${user.name}, \n\nUse this code to reset your password:\n\nSecret:${token.token}\n\nThank You :D`
+  }
+
+}
+
+
+export default {
+	verificationTemplate,
+	passwordResetTemplate
 }
